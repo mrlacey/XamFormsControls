7cba3d233ae13d75a014d9f7c15d119b7f706c88
Matt Lacey <github@mrlacey.co.uk>
2015-08-14 18:53:44 +0100

    Early basics

---
+++ XamFormsControls/XamFormsControls/XamFormsControls/MyContentPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace XamFormsControls
+{
+    public partial class MyContentPage : ContentPage
+    {
+        public MyContentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MasterDetailClicked(object sender, EventArgs eventArgs)
+        {
+        }
+
+        private void NavigationClicked(object sender, EventArgs eventArgs)
+        {
+        }
+
+        private void TabbedClicked(object sender, EventArgs eventArgs)
+        {
+        }
+
+        private void CarouselClicked(object sender, EventArgs eventArgs)
+        {
+        }
+    }
+}
